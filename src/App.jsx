6c6a96a2b6962268c071cf270e77bf7d675f7786
Daniel Hayward <daniel.hayward@keyin.com>
2023-12-21 00:24:06 -0330

    finished (for now).

--- src/App.jsx
+++ src/App.jsx
@@ -16,13 +16,40 @@ function App() {
       <div className="top-container">
         <div className={switchCheck(about, "info-box", "info-box-hidden")}>
           <h2>About Me</h2>
-          <p></p>
+          <p>
+            Hello! I'm a 21 year old student, currently enrolled in the Computer
+            Science program at Keyin College.
+          </p>
+          <br />
+          <p>
+            I've been interested in computers and technology my entire life.
+            Always searching for some sort of challenge, something to put my
+            knowledge and experience to the test.
+          </p>
+          <br />
+          <p>
+            After finally receiving official education, I'm certain I've found
+            my direction.
+          </p>
         </div>
         <div className={switchCheck(skills, "info-box", "info-box-hidden")}>
           <h2>Skills</h2>
+          <p>proficient in various programming languages such as:</p>
+          <ul>
+            <li>Python</li>
+            <li>JS</li>
+            <li>HTML and CSS</li>
+            <li>The list keeps growing...</li>
+          </ul>
+          <br />
+          <p>
+            I've also taken the <span className="important">AWS Academy</span>{" "}
+            course as a part of my program.
+          </p>
         </div>
       </div>
       <div className="bottom-container">
+        <h1>Daniel Hayward</h1>
         <h1>Portfolio</h1>
         <h2>Select any options</h2>
         <div className="switch-container">
@@ -60,7 +87,7 @@ function App() {
               <div
                 className={switchCheck(contact, "circle-clicked", "circle")}
               ></div>
-              <p>About Me</p>
+              <p>Contact</p>
             </div>
           </div>
         </div>
@@ -68,6 +95,7 @@ function App() {
           className={switchCheck(contact, "contact-box", "contact-box-hidden")}
         >
           <h2>Contact</h2>
+          <p>Email: danielkevinhayward@outlook.com</p>
         </div>
       </div>
     </div>
